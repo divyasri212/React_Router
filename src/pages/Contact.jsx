e8c768b41b8c0bcc490f948870e699b449f49439
Divya <divyasriainampudi@gmail.com>
2024-10-28 10:07:20 +0530

    first commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Contact = () => (
+    <div>
+        <h2>Contact Us</h2>
+        <p>Get in touch with us for any inquiries or support.</p>
+    </div>
+);
+
+export default Contact;
